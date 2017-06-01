287c5fa416dee091b270bb94bb97fd4841539bea
GAURAV SINGH <gauravsinghf22@gmail.com>
2017-05-31 20:55:10 -0500

    Update Gaurav.R

--- Gaurav.R
+++ Gaurav.R
@@ -1,3 +1,20 @@
+#Python file
+import subprocess
+a=input("Enter your name to proceed with analysis: ")
+if (a=="gaurav"):
+    #subprocess.call("D:\\scripting project\\library.R", shell=True)
+    #subprocess.call("D:\\scripting project\\Net.R", shell=True)
+    subprocess.call("D:\\scripting project\\Gaurav.R", shell=True)
+elif(a=="naveen"):
+    subprocess.call("D:\scripting project\\Naveen.R", shell=True)
+elif (a=="seema"):
+    subprocess.call("D:\scripting project\\Seema.R", shell=True)
+elif(a=="goutham"):
+    subprocess.call("D:\scripting project\\Goutham.R", shell=True)
+else:
+    print("Invalid User")
+
+#R GUI file
 #loading all the libraries
 library(data.table)
 library(dplyr)
